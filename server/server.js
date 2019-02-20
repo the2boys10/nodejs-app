057ddd91ba747c6af2fc58728f6e4b40c8dead43
Robert Johnson <Robert.Johnson@crowderconsult.co.uk>
2019-02-20 16:32:48 +0000

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -14,9 +14,9 @@ app.post("/todos",(req,res)=>{
     todo.save().then((doc)=>{res.send(doc)},(err)=>{res.status(400).send(err)});
 });
 
-// app.get("/todos",(req,res)={
-
-// })
+app.get("/todos",(req,res)=>
+    Todo.find().then((todos)=>{res.send({todos})},(e)=>{res.status(400).send(err)})
+);
 
 app.listen(3000,()=>{
     console.log("Started server on port 3000");
