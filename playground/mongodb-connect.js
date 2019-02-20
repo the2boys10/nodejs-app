1d30ff9256cb8fb50434d63c68e2317de599bece
Robert Johnson <Robert.Johnson@crowderconsult.co.uk>
2019-02-20 14:22:42 +0000

    Forgot to save connect

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,7 +1,5 @@
 const {MongoClient, ObjectID} = require('mongodb');
 
-var obj = new ObjectID();
-console.log(obj);
 MongoClient.connect('mongodb://localhost:27017/TodoApp',(error,client)=>{
         if(error){
             return console.log('Unable to connect to mongodb server');
