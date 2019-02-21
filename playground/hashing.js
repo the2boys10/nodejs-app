1d25a7c3fef32b5d8507a4ab756995e7684a0457
Robert Johnson <Robert.Johnson@crowderconsult.co.uk>
2019-02-21 15:56:10 +0000

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,36 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data,'123abc');
-console.log(token);
-var decoded = jwt.verify(token,"123abc");
-console.log('docoded',decoded);
+bcrypt.genSalt(10,(err, salt)=>{
+    bcrypt.hash(password,salt,(err, hash)=>{
+        console.log(hash);
+    });
+});
+
+
+var hashedPassword = '$2a$10$hY6QXcCjShyUpuBdP3TcEefSclJNcbpc403zfasWvoUahH4NlLaX.';
+bcrypt.compare("123abc!", hashedPassword,(err,res)=>{
+    console.log(res);
+});
+
+
+
+
+
+
+
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+// var decoded = jwt.verify(token,"123abc");
+// console.log('docoded',decoded);
 
 
 
