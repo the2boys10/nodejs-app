61b3d516806df9891a5d61ec282b74fe617b279c
Robert Johnson <Robert.Johnson@crowderconsult.co.uk>
2019-02-21 11:05:44 +0000

    Setup seperate test and development envs

---
+++ server/config/config.js
@@ -0,0 +1,12 @@
+var env = process.env.NODE_ENV || 'development';
+
+if(env === 'development'){
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else {
+    process.env.MONGODB_URI = 'mongodb+srv://the2boys10:mywebsitepassword@cluster0-qjn8e.mongodb.net/test?retryWrites=true';
+}
+
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
-// mongoose.connect('mongodb+srv://the2boys10:mywebsitepassword@cluster0-qjn8e.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 module.exports = {mongoose};
+
+process.env.NODE_ENV === 'test';
