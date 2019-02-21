fcca6dd1e5db85349e83c1382dcf501d08bb16b9
Robert Johnson <Robert.Johnson@crowderconsult.co.uk>
2019-02-21 10:16:52 +0000

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 const port = process.env.PORT || 3000;
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/Todo');
@@ -41,6 +42,29 @@ app.delete("/todos/:id",(req,res)=>{
         return res.status(404).send();
     }
     Todo.findByIdAndDelete({_id: req.params.id}).then(
+        todo=>{
+            if(!todo){
+                return res.status(404).send();
+            }
+            res.send({todo});
+        },
+        err=>res.status(400).send(err)
+    )
+});
+
+app.patch("/todos/:id",(req,res)=>{
+    if(!ObjectID.isValid(req.params.id)){
+        return res.status(404).send();
+    }
+    var body = _.pick(req.body,['text','completed']);
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(req.params.id,{$set:body},{new:true}).then(
         todo=>{
             if(!todo){
                 return res.status(404).send();
