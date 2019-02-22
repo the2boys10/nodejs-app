6e4b9611f7415129ec522b86faa3d547754a98c7
Robert Johnson <Robert.Johnson@crowderconsult.co.uk>
2019-02-22 09:57:37 +0000

    Add Delete/me/token route

--- server/models/User.js
+++ server/models/User.js
@@ -52,6 +52,11 @@ UserSchema.methods.toJSON = function() {
     return userObject;
 };
 
+UserSchema.methods.removeToken = function(token) {
+    var user = this;
+    return user.update({$pull: {tokens:{token}}});
+};
+
 UserSchema.statics.findByToken = function(token) {
     var User = this;
     var decoded;
